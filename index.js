fff0065aa3d1ac5f0616bb9da24f8de491088d5b
James <jamescamerontaylor20@gmail.com>
2024-05-07 19:55:07 +1000

    Generator done just need to clean up a bit

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const path = require("path");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please name your project.",
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter your name.",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe the project.",
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "Please describe the Installation process.",
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "Please enter usage information.",
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Please select license.",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+    },
+    {
+        type: "input",
+        name: "Contribution",
+        message: "Please list contributions.",
+        default: "",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please list required tests if needed.",
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please provide your github.",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please provide your email.",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(),fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) =>{
+        console.log("Creating README.md File...");
+        writeToFile("./dist/README.md", generateMarkdown({...responses}));
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,57 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+function renderLicenseSection(license) {
+  if (license !== "none") {
+    return `## License
+      Licensed under the ${license} license.`;
+  }
+  return "";
+}
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
   return `# ${data.title}
+by ${data.name}
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Table of Contents
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contibutions](#Contributions)
+* [Testing](#Testing)
+* [Questions](#Questions)
+## Installation
+${data.Installation}
+## Usage
+${data.Usage}
+## Contributions
+${data.Contribution}
+## Testing
+${data.test}
+## Questions 
+My Github is at (https://www.github.com/${data.github})
 
+
+If you have any questions please contact me at ${data.email}
+
+${renderLicenseSection(data.license)}
 `;
 }
 
